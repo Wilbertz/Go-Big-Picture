1238f28c3b6e5952f617c4d06fe1af9f08019c0f
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-07-11 19:31:52 +0200

    First web server.

--- main.go
+++ main.go
@@ -1,41 +1,17 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
 	"log"
-	"os"
-	"strings"
+	"net/http"
 )
 
 func main() {
-	path := flag.String("path", "myapp.log", "The path to the log.")
-	level := flag.String("level", "ERROR", "Log Level to search for.")
-
-	flag.Parse()
-
-	f, err := os.Open(*path)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Harald"))
+	})
+	err := http.ListenAndServe(":3000", nil)
 
 	if err != nil {
 		log.Fatal(err)
-	}
-	defer func(f *os.File) {
-		err := f.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(f)
-
-	r := bufio.NewReader(f)
-
-	for {
-		s, err := r.ReadString('\n')
-		if err != nil {
-			break
-		}
-		if strings.Contains(s, *level) {
-			fmt.Println(s)
-		}
 	}
 }
