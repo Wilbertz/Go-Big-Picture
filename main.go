c24ff749ba8caf1088c1c1474f639928548d945d
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-07-11 20:29:31 +0200

    Json string returned.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -13,7 +14,9 @@ func main() {
 		if len(names) == 1 {
 			name = names[0]
 		}
-		w.Write([]byte("Hello " + name))
+		m := map[string]string{"name": name}
+		enc := json.NewEncoder(w)
+		enc.Encode(m)
 	})
 	err := http.ListenAndServe(":3000", nil)
 
